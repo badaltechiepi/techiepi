a81359f26522d9a1478ba688965eca618af841f1
badaltechiepi <badalkumarsahu92@gmail.com>
2020-10-17 02:13:12 +0530

    adding the rest subnet

--- VPC/main.tf
+++ VPC/main.tf
@@ -6,7 +6,7 @@ resource "aws_vpc" "myvpc" {
   }
 }
 
-#creating the subnets as discussed
+#creating the subnets for web
 
 resource "aws_subnet" "web"{
   cidr_block    = "192.168.0.0/24"
@@ -15,4 +15,32 @@ resource "aws_subnet" "web"{
   Name          = "web"
   }
 }
+#creating the subnets for app
 
+resource "aws_subnet" "app"{
+  cidr_block    = "192.168.1.0/24"
+  vpc_id        = aws_vpc.myvpc.id
+  tags = {
+  Name          = "app"
+  }
+}
+
+#creating the subnets for db
+
+resource "aws_subnet" "db"{
+  cidr_block    = "192.168.2.0/24"
+  vpc_id        = aws_vpc.myvpc.id
+  tags = {
+  Name          = "db"
+  }
+}
+
+#creating the subnets for mgmt
+
+resource "aws_subnet" "mgmt"{
+  cidr_block    = "192.168.3.0/24"
+  vpc_id        = aws_vpc.myvpc.id
+  tags = {
+  Name          = "mgmt"
+  }
+}
