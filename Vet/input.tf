8d5314a1df46296024dbe4018b4dcad324d2700a
badaltechiepi <badalkumarsahu92@gmail.com>
2020-10-18 18:25:13 +0530

    adding the vnet script

---
+++ Vet/input.tf
@@ -0,0 +1,5 @@
+variable location {
+  type        = string
+  default     = "East US 2"
+  description = "location to create the vnet"
+}
--- Vet/main.tf
+++ Vet/main.tf
@@ -1,6 +1,25 @@
 resource "azurerm_resource_group" "ntire"{
 
     name = "ntire"
-    location = "East US 2"
+    location = var.location
+    tags            = {
+        Purpose     = "Learning",
+        CreatedBy   = "Terraform"
+    }
+
+}
+
+resource azurerm_virtual_network ntire_vnet{
+
+    name = "firstvnet"
+    resource_group_name = azurerm_resource_group.ntire 
+    address_space = var.addressspace
+    location = var.location
+    tags            = {
+        Purpose     = "Learning",
+        CreatedBy   = "Terraform"
+    }
+
+
 
 }
