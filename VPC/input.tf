8b29617878e26a2994db97014efa201108cb8b01
badaltechiepi <badalkumarsahu92@gmail.com>
2020-10-18 13:06:35 +0530

    all the generic automtion done

--- VPC/input.tf
+++ VPC/input.tf
@@ -1,11 +1,30 @@
+#this varialbe defin the region
 variable "region" {
   type        = string
   default     = "us-east-2"
   description = "region"
 }
 
-variable vpc {
+#this variable define the cidr_block(ip range)
+variable cidr_block {
   type        = string
-  default     = "192.168.0.0/16"
+  default     = "10.0.0.0/16"
   description = "cidr_block value"
 }
+
+#thid variable defind to get the vpc id(attribute)
+variable vpc_id {
+  type        = string
+  default     = "aws_vpc.myvpc.id"
+  description = "vpc id"
+}
+
+#tag name
+variable tagname {
+  type        = list(string)
+  default     = ["myfistvpc","web","app","db","mgmt"]
+  description = "tag names"
+}
+
+
+
--- VPC/main.tf
+++ VPC/main.tf
@@ -1,46 +1,52 @@
 #creatig the VPC in aws
 resource "aws_vpc" "myvpc" {
-  cidr_block        = var.vpc
+  cidr_block        = var.cidr_block
   tags = {
-      Name      = "fistvpc"
+      Name      = var.tagname[0]
   }
 }
 
 #creating the subnets for web
 
 resource "aws_subnet" "web"{
-  cidr_block    = cidrsubnet("10.0.0.0/16", 8, 0)
-  vpc_id        = aws_vpc.myvpc.id
+  cidr_block    = cidrsubnet(var.cidr_block, 8, 0)
+  vpc_id        = var.vpc_id
+  availability_zone       = format("%sa", var.region)
   tags = {
-  Name          = "web"
+  Name          = var.tagname[1]
   }
 }
 #creating the subnets for app
 
+
 resource "aws_subnet" "app"{
-  cidr_block    = cidrsubnet("10.0.0.0/16", 8, 1)
-  vpc_id        = aws_vpc.myvpc.id
+  cidr_block    = cidrsubnet(var.cidr_block, 8, 1)
+  vpc_id        = var.vpc_id
+  availability_zone       = format("%sb", var.region)
   tags = {
-  Name          = "app"
+  Name          = var.tagname[2]
   }
 }
 
 #creating the subnets for db
 
 resource "aws_subnet" "db"{
-  cidr_block    = cidrsubnet("10.0.0.0/16", 8, 2)
-  vpc_id        = aws_vpc.myvpc.id
+  cidr_block    = cidrsubnet(var.cidr_block, 8, 2)
+  vpc_id        = var.vpc_id
+  availability_zone       = format("%sc", var.region)
+  
   tags = {
-  Name          = "db"
+  Name          = var.tagname[3]
   }
 }
 
 #creating the subnets for mgmt
 
 resource "aws_subnet" "mgmt"{
-  cidr_block    = cidrsubnet("10.0.0.0/16", 8, 3)
-  vpc_id        = aws_vpc.myvpc.id
+  cidr_block    = cidrsubnet(var.cidr_block, 8, 3)
+  vpc_id        = var.vpc_id
+  availability_zone       = format("%sa", var.region)
   tags = {
-  Name          = "mgmt"
+  Name          = var.tagname[4]
   }
 }
