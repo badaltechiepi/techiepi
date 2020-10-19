cd2b93b31c3beafe215cb51c42ce01af07b9cf5e
badaltechiepi <badalkumarsahu92@gmail.com>
2020-10-19 22:14:27 +0530

    adding the datasource to the file

--- VPC_loop/network.tf
+++ VPC_loop/network.tf
@@ -4,6 +4,7 @@ resource "aws_vpc" "myvpc"{
     Name = "ntier primary"
   }
 }
+#lengthfunction doesn't has squre bracket
 resource "aws_subnet" "mysubnet"{
     vpc_id     = aws_vpc.myvpc.id
     count      = length(var.subnets)
@@ -11,6 +12,22 @@ resource "aws_subnet" "mysubnet"{
      tags = {
     Name = var.subnets[count.index]
   }
+}
+#data source example
+data "aws_vpc" "selected" {
+  cidr_block = var.cidr_block
+}
+
+#creating a extra subnet
+
+resource "aws_subnet" "extra"{
+
+    vpc_id = data.aws_vpc.selected.cidr_block
+    cidr_block        = cidrsubnet(data.aws_vpc.selected.cidr_block, 8, 4)
+    tags = {
+    Name = "extra"
+  }
+
 }
 
 
